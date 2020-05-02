c8dd1c6d5f05ade435e5c23dfcad0ef28c454fa6
wellingtoncalixto <wac.monti@outlook.com>
2020-05-02 00:00:31 -0300

    Desafio 7 completo

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
+import { zonedTimeToUtc } from 'date-fns-tz';
 import income from '../../assets/income.svg';
 import outcome from '../../assets/outcome.svg';
 import total from '../../assets/total.svg';
-
 import api from '../../services/api';
 
 import Header from '../../components/Header';
@@ -30,12 +30,14 @@ interface Balance {
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      // TODO
+      const response = await api.get('transactions');
+      setTransactions(response.data.transactions);
+      setBalance(response.data.balance);
     }
 
     loadTransactions();
@@ -51,21 +53,27 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">
+              {`${formatValue(parseInt(balance.income, 0))}`}
+            </h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">
+              {`${formatValue(parseInt(balance.outcome, 0))}`}
+            </h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">
+              {`${formatValue(parseInt(balance.total, 0))}`}
+            </h1>
           </Card>
         </CardContainer>
 
@@ -81,18 +89,26 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions &&
+                transactions.map(transaction => (
+                  <tr key={transaction.id}>
+                    <td className="title">{transaction.title}</td>
+                    <td className={`${transaction.type}`}>
+                      {`${
+                        transaction.type === 'outcome' ? '- ' : ''
+                      }${formatValue(transaction.value)}`}
+                    </td>
+                    <td>{transaction.category.title}</td>
+                    <td>
+                      {new Intl.DateTimeFormat('pt-BR').format(
+                        zonedTimeToUtc(
+                          transaction.created_at,
+                          'America/Sao_Paulo',
+                        ),
+                      )}
+                    </td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </TableContainer>
--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,29 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.map(uploadedFile => data.append('file', uploadedFile.file));
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.error(err.response.error);
     }
+
+    history.push('/');
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    files.map(file => {
+      const newFile: FileProps = {
+        file,
+        name: file.name,
+        readableSize: String(file.size),
+      };
+
+      setUploadedFiles([...uploadedFiles, newFile]);
+    });
   }
 
   return (
